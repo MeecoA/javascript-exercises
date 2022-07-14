25042fd432ad08a58e880883a0359e357b349110
Meeco <meecorcll@gmail.com>
2022-07-14 19:03:03 +0800

    javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    var repeatStr = "";
 
+    while (num>0){
+        repeatStr=repeatStr+string; 
+        num--; 
+    }
+    return repeatStr; 
+    
+}
 // Do not edit below this line
 module.exports = repeatString;
