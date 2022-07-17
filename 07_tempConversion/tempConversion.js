3af924ad4d6548e9e686c52b9378db0c5b5c876d
Meeco <meecorcll@gmail.com>
2022-07-18 07:52:29 +0800

    Finished JS exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+
+  const conversion =   (tempF - 32) * 5/9 ; 
+  
+  const roundedConversion =  Math.round(conversion * 10) / 10 ; 
+
+  return roundedConversion; 
 
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  const conversion =   (tempC*9/5)+ 32 ; 
+  
+  const roundedConversion =  Math.round(conversion * 10) / 10 ; 
+
+  return roundedConversion; 
 
 };
 
